e159b25252e37dbc558e130dddf8c429faf6182e
Annette Bieniusa <bieniusa@cs.uni-kl.de>
2015-05-09 15:36:08 +0200

    Removed warning.

--- Programmierpraktikum/src/BudgetPlan.java
+++ Programmierpraktikum/src/BudgetPlan.java
@@ -1,8 +1,7 @@
-
 public class BudgetPlan {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		BudgetPlanModel budget = new BudgetPlanModel();
-		BudgetPlanGUI theAppWindow = new BudgetPlanGUI(budget);
+		new BudgetPlanGUI(budget);
 	}
-	
+
 }
